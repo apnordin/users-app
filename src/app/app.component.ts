bedd538a4abbb84c7a6bf4ccc9e275e00478bce5
Alex Nordin <apnordin@gmail.com>
2022-01-18 17:55:21 -0500

    First commit, forms and button, nothing wired up to firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'users-app';
+  newUser = {};
+  newUsername = "";
+  newAddress = "";
+  newPhone = "";
+  newEmail = "";
+  newAge: number | undefined = undefined;
+  newExperience = "";
+  newEmployment = "";
+
+  onUsernameInput(username: string) {
+    this.newUsername = username;
+  }
+
+  onAddressInput(address: string) {
+    this.newAddress = address;
+  }
+  
+  onPhoneInput(phone: string) {
+    this.newPhone = phone;
+  }
+
+  onEmailInput(email: string) {
+    this.newEmail = email;
+  }
+   onAgeInput(age: string) {
+    this.newAge = Number(age);
+  }
+
+  onExperienceInput(experience: string) {
+    this.newExperience = experience;
+  }
+
+  onEmploymentInput(employment: string) {
+    this.newEmployment = employment;
+  }
+
+  addUser() {
+    this.newUser = {
+      username: this.newUsername,
+      address: this.newAddress,
+      phone: this.newPhone,
+      email: this.newEmail,
+      age: this.newAge,
+      experience: this.newExperience,
+      employment: this.newEmployment,
+    }
+    console.log(this.newUser)
+  }
+
+
 }
